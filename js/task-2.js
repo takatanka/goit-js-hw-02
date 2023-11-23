3002f97b66a4ee7ffc7d1e4c6c893e604802dc9b
takatanka <tashka.falcon@gmail.com>
2023-11-23 22:29:19 +0100

    ver.first

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+function formatMessage(message, maxLength) {
+    let cuttedMessage;
+    if (message.length <= maxLength) {
+        cuttedMessage = message;
+    } else {
+        cuttedMessage = message.slice(0, maxLength) + '...';
+}
+return cuttedMessage;
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
